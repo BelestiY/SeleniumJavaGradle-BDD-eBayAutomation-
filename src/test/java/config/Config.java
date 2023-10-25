517e28d0968a0cc13fb49ae4a609dfcbe26439c5
Belesti Yenegeta <belestibdu@gmail.com>
2023-10-25 11:20:11 +0300

    SeleniumJava - Gradle - Automation Test of eBay commercial site

---
+++ src/test/java/config/Config.java
@@ -0,0 +1,11 @@
+package config;
+
+public class Config {
+    public static final String browser="edge"; // chrome, edge, firefox
+
+    public static String ebayURL = "https://www.ebay.com/";
+
+
+
+
+}
